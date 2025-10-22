266a06b5cb9c7d038ff7f49e632af9f5677f42d6
Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>
2025-10-22 13:07:26 +0200

    feat(qti): integrate sec_core stub
    Add the sec_core driver stub and hook it into the generic QTI driver
    framework. The stub remains disabled while QTISECLIB continues to
    provide the active implementation.
    
    For coverity purposes, enable the driver on the non-bootable builds for
    the kodiak/rb3gen2 platform.
    
    Change-Id: I83a0afd0a2f89d0fe31b687f4341d9bb8518ec8d
    Signed-off-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>

--- plat/qti/kodiak/rb3gen2/platform.mk
+++ plat/qti/kodiak/rb3gen2/platform.mk
@@ -102,10 +102,12 @@ ifeq ($(QTISECLIB_PATH),)
 $(warning QTISECLIB_PATH is not provided while building, using stub implementation. \
 		Please refer to documentation for more details \
 		THIS FIRMWARE WILL NOT BOOT!)
-BL31_SOURCES	+=	plat/qti/qtiseclib/src/qtiseclib_interface_stub.c
+BL31_SOURCES	+=	plat/qti/qtiseclib/src/qtiseclib_interface_stub.c \
+			drivers/qti/sec_core/sec_core.c
 else
 $(eval $(call add_define,QTISECLIB_PATH))
 # use library provided by QTISECLIB_PATH
+BL31_SOURCES	+=			drivers/qti/sec_core/sec_core_stub.c
 LDFLAGS += -L $(dir $(QTISECLIB_PATH))
 LDLIBS += -l$(patsubst lib%.a,%,$(notdir $(QTISECLIB_PATH)))
 endif
