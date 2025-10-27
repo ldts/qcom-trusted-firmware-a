5364eeb8b454ad3926ad2428c32895bf8cae53a7
Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>
2025-10-27 17:37:46 +0100

    feat(qti/kodiak): add qtimer driver
    Introduce a minimal QTimer driver implementing basic initialization, raw
    counter access, and a microsecond conversion helper.
    
    Change-Id: I8dbde26848642b697302508847a7812e32d146e0
    Signed-off-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>

---
+++ plat/qti/kodiak/inc/qti_qtimer.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef QTI_QTIMER_H
+#define QTI_QTIMER_H
+
+#define QTIMER_BASE_ADDRESS		0x17C20000
+#define QTIMER0_F2V1_BASE_ADDRESS	QTIMER_BASE_ADDRESS + 0x5000
+
+#define QTIMER_NBR_FRAMES		7
+
+#endif /* QTI_QTIMER_H */
